0c1da6e7db502da8dac2d00b9ac319cd74d971fd
suragch <studymongolian@gmail.com>
2019-08-22 17:18:03 +0800

    exposed Scripts class

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Change log
 
+
+## 0.3.0
+
+- Exposed `Scripts` class
+
 ## 0.2.0
 
 - Added a script list `UnicodeScript.scripts`
--- lib/unicode_scripts_blocks.dart
+++ lib/unicode_scripts_blocks.dart
@@ -3,3 +3,4 @@ library unicode_scripts_blocks;
 export 'src/unicode_block.dart';
 export 'src/unicode_script.dart';
 export 'src/unicode_script_2.dart';
+export 'src/script.dart';
